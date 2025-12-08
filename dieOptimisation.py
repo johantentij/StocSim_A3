f37658af4893631e40ecafba04ac2193e4a0e1ba
johantentij <johantentij@gmail.com>
2025-12-09 00:30:37 +0100

    Added winning percentage and simulated annealing

--- dieOptimisation.py
+++ dieOptimisation.py
@@ -21,6 +21,24 @@ ladders = np.array([[1, 38],
                     [71, 91],
                     [80, 99]])
 
+# more cheating resistant board
+# snakes = np.array([[27,  7],
+#                    [50, 30],
+#                    [55, 35],
+#                    [68, 48],
+#                    [71, 61],
+#                    [77, 67],
+#                    [94, 84]])
+
+# ladders = np.array([[4, 25],
+#                     [10, 32],
+#                     [36, 52],
+#                     [43, 80],
+#                     [46, 66],
+#                     [63, 73],
+#                     [64, 83],
+#                     [75, 85]])
+
 jumps = np.concatenate((snakes, ladders))
 
 T_terms = []
@@ -28,7 +46,7 @@ for k in range(6):
     T = np.zeros((107, 101))
 
    
-    for j in range(100):
+    for j in range(101):
         jumpPos = (j + k + 1 == jumps[:, 0])
         if np.sum(jumpPos):
             T[jumps[jumpPos, 1], j] = 1
@@ -44,6 +62,40 @@ overshoot = sp.csr_array(overshoot)
 makeSquare = np.concatenate((np.identity(101), np.zeros((101, 6))), axis=1)
 makeSquare = sp.csr_array(makeSquare)
 
+def makeDistribution(p, maxThrows=200):
+    T = sp.csr_array((107, 101))
+
+    for i in range(6):
+        T += p[i] * T_terms[i]
+
+    overshootVector = overshoot.dot(T).toarray()
+    overshootVector[-1] = 0
+    T = makeSquare.dot(T) + sp.diags(overshootVector, 0)
+
+    v = np.zeros(101)
+    v[0] = 1
+
+    dist = np.zeros(maxThrows)
+    i = 0
+    for i in range(maxThrows):
+        v = T.dot(v)
+        dist[i] = v[-1]
+
+        i += 1
+
+    dist[i:] = 0
+
+    return dist
+
+def winningPercentage(pmfWinner, pmfLoser):
+    cdfWinner = np.cumsum(pmfWinner)
+
+    prob = 0
+    for i in range(1, np.shape(pmfLoser)[0]):
+        prob += pmfLoser[i] * (cdfWinner[i - 1] + .5 * pmfWinner[i])
+
+    return prob
+
 def exactExpectedLength(p):
     T = sp.csr_array((107, 101))
     I = sp.diags(np.ones(100), 0)
@@ -58,44 +110,92 @@ def exactExpectedLength(p):
 
     return E[0]
 
-def step(prevL, p, perturbation, temperature):
+def step(prevAdvantage, basePmf, p, perturbation, temperature):
     # propose a new die
     proposed = p + np.random.normal(0, perturbation, 6)
     proposed[proposed < 0] = 0
     proposed /= np.sum(proposed)
 
-    newL = exactExpectedLength(proposed)
+    newPmf = makeDistribution(proposed)
+    newAdvantage = winningPercentage(newPmf, basePmf)
 
-    alpha = np.min((1, np.exp(-(newL - prevL) / temperature)))
+    alpha = np.min((1, np.exp((newAdvantage - prevAdvantage) / temperature)))
 
     if np.random.rand() <= alpha:
-        return newL, proposed
+        return newAdvantage, proposed
     else:
-        return prevL, p
+        return prevAdvantage, p
     
-def MetropolisHastings(p_0, perturbation, steps=1000, temperature=10):
+def MetropolisHastings(p_0, perturbation=1e-2, steps=1000, gradientFunc=None, temp=1e-3):
+    if (gradientFunc is None):
+        # if no gradient is given use constant temperature
+        gradientFunc = lambda x : temp
+
     p = p_0 / np.sum(p_0)
 
-    gameLengths = np.empty(steps)
-    gameLengths[0] = exactExpectedLength(p)
+    basePmf = makeDistribution(p)
+    advantages = np.empty(steps)
+    advantages[0] = .5
+
     for i in range(1, steps):
-        gameLengths[i], p = step(gameLengths[i - 1], p, perturbation, temperature)
+        # propose a new die
+        proposed = p + np.random.normal(0, perturbation, 6)
+        proposed[proposed < 0] = 0
+        proposed /= np.sum(proposed)
 
-    return gameLengths, p
+        newPmf = makeDistribution(proposed)
+        newAdvantage = winningPercentage(newPmf, basePmf)
 
-gameLengths, p = MetropolisHastings(np.ones(6), 1e-2, temperature=0.1)
+        temp = gradientFunc(i)
+        alpha = np.min((1, np.exp((newAdvantage - advantages[i - 1]) / temp)))
 
-plt.plot(gameLengths[1:])
+        if np.random.rand() <= alpha:
+            p = proposed
+            advantages[i] = newAdvantage
+        else:
+            advantages[i] = advantages[i - 1]
+
+    return advantages, p
+
+def expGradient(i, steps, start, end):
+    return start * np.power(end / start, i / (steps - 1))
+
+def linGradient(i, steps, start, end):
+    return start + (end - start) * i / (steps - 1)
+
+gradientFunc = lambda i : expGradient(i, 1000, 1e-2, 1e-4)
+# gradientFunc = lambda i : linGradient(i, 1000, 1e-1, 1e-4)
+
+advantages, p = MetropolisHastings(np.ones(6), steps=1000, gradientFunc=gradientFunc)
+
+meanThrow = np.sum((np.arange(6) + 1) * p)
+
+# simulated annealing plot
+plt.plot(advantages[1:])
 plt.title("Metropolis-Hastings")
 plt.xlabel("Steps")
-plt.ylabel("Expected game length")
+plt.ylabel("Winning chance with biased die")
 plt.figure()
 
 plt.bar(np.arange(6) + 1, p)
+plt.vlines(meanThrow, 0, np.max(p), linestyle="dashed", color="grey")
 plt.title("PMF of optimised die")
 plt.xlabel("Roll")
 plt.ylabel("Probability")
 plt.show()
+
+# plot game length PMF distributions
+# distRef = makeDistribution(np.ones(6) / 6)
+# dist = makeDistribution(p)
+
+# print(winningPercentage(p, np.ones(6) / 6))
+
+# plt.plot(distRef, label="fair die")
+# plt.plot(dist, label="optimised die")
+# plt.xlabel("Number of turns")
+# plt.ylabel("Probability of game ending")
+# plt.legend()
+# plt.show()
 
 
 
